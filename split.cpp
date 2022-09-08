f80229a550888e798f0bc72fdffa0a1694da48f2
Jake Martin <jakemart@usc.edu>
2022-09-08 08:06:36 +0000

    Code changes for problem 1

--- split.cpp
+++ split.cpp
@@ -9,7 +9,7 @@ To test your program write a separate .cpp file and #include
 split.h.  **Do NOT add main() to this file**.  When you submit
 the function below should be the only one in this file.
 */
-
+#include <cstddef>
 #include "split.h"
 
 /* Add a prototype for a helper function here if you need */
@@ -19,7 +19,29 @@ void split(Node*& in, Node*& odds, Node*& evens)
   /* Add code here */
 // WRITE YOUR CODE HERE
 
-}
+
+  if (in == NULL){
+    in = nullptr;
+    evens = nullptr;
+    odds = nullptr;
+    return;
+
+  }
+  if (in->value % 2 == 0){
+    // evens->next->value = in->value;
+    evens = in;
+    return split(in->next, odds, evens->next);
+
+  }
+  if (in->value % 2 != 0){
+    // odds->next->value = in->value;
+    odds = in;
+    return split (in->next, odds->next, evens);
+  }
+    
+   
+  }
+  
 
 /* If you needed a helper function, write it here */
 
--- split_test.cpp
+++ split_test.cpp
@@ -6,32 +6,54 @@ using namespace std;
 
 int main() {
 
+
   Node* head = NULL;
   Node* second = NULL;
   Node* third = NULL;
   Node* tail = NULL;
 
-  head->value = 0;
-  head->next = second;
+  tail = new Node (3, NULL);
+  third = new Node (2, tail);
+  second = new Node (1, third);
+  head = new Node (0, second);
 
-  second->value = 1;
-  second->next = third;
+  // head->value = 0;
+  // head->next = second;
 
-  third->value = 2;
-  third->next = tail;
+  // second->value = 1;
+  // second->next = third;
 
-  tail->value = 3;
-  tail->next = NULL;
+  // third->value = 2;
+  // third->next = tail;
+
+  // tail->value = 3;
+  // tail->next = NULL;
 
   Node* evens = NULL;
   Node* odds = NULL; 
-
 split(head, odds, evens);
 
-cout << evens->value << endl;
-cout << evens->next->value << endl;
-cout << odds->value << endl;
-cout << odds->next->value << endl;
+Node* temp;
+temp = evens;
+while (temp != NULL){
+  cout << temp->value << endl;
+  temp = temp->next;
+}
+Node* temp2;
+temp2 = odds;
+while (temp2 != NULL) {
+  cout << temp2->value << endl;
+  temp2 = temp2->next;
+}
 
+// cout << evens->value << endl;
+// cout << evens->next->value << endl;
+// cout << odds->value << endl;
+// cout << odds->next->value << endl;
+
+delete head;
+delete second;
+delete third;
+delete tail;
 
 }
